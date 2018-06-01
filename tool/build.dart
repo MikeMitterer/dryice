4e695b0ec0c6685d49bfc12f8529f6440a264257
Mike Mitterer <office@mikemitterer.at>
2018-06-01 12:18:01 +0200

    Removed unnecessary imports

--- tool/build.dart
+++ tool/build.dart
@@ -1,10 +1,3 @@
-// Copyright (c) 2017, the Dart Team. All rights reserved. Use of this
-// source code is governed by a BSD-style license that can be found in
-// the LICENSE file.
-import 'dart:async';
-
-import 'package:build_runner/build_runner.dart';
-import 'package:glob/glob.dart';
 import 'package:reflectable/reflectable_builder.dart' as builder;
 
 const String _DEFAULT_ENTRY_POINT = "web/main.dart";
