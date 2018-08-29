633fc45a878f67c0a163679bde3e17575fa33ca5
Mike Mitterer <office@mikemitterer.at>
2018-08-29 10:12:46 +0200

    reorg: Removed annotations.dart and moved InjectAnnotation to dryice.dart

--- lib/dryice.dart
+++ lib/dryice.dart
@@ -10,12 +10,32 @@ import 'dart:collection';
 import 'package:reflectable/reflectable.dart';
 import 'package:logging/logging.dart';
 
-part 'src/annotations.dart';
 part 'src/assert_messages.dart';
 part 'src/injector.dart';
 part 'src/mirror_util.dart';
 part 'src/module.dart';
 part 'src/Registration.dart';
+
+class InjectAnnotation extends Reflectable {
+    const InjectAnnotation() : super(
+
+        // instanceInvokeCapability,
+        invokingCapability,
+        reflectedTypeCapability,
+        typeCapability,
+        typingCapability,
+        metadataCapability,
+        // newInstanceCapability,
+    );
+}
+
+/// Used in conjunction with [Inject] to select a specific named target for injection
+class Named {
+    const Named(this.name);
+
+    final String name;
+}
+
 
 /// Compatibility to JSR-330
 /// https://github.com/google/guice/wiki/JSR330
